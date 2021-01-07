e8078f8007c187a8e4592df957bc1658f80769e1
Kou Honda <kou_honda@eastback.jp>
2021-01-07 22:58:19 +0900

    Add test2

---
+++ src/test/java/MvcSample/controller/TDB1002ControllerTest.java
@@ -0,0 +1,40 @@
+package MvcSample.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import MvcSample.config.WebMvcConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { WebMvcConfig.class })
+@WebAppConfiguration
+public class TDB1002ControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void givenHomePageURI_whenMockMVC_thenReturnsIndexJSPViewName() throws Exception {
+        this.mockMvc.perform(get("/TDB1002")).andDo(print())
+
+          .andExpect(view().name("TDB1002"));
+    }
+}
